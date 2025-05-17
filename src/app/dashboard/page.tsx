2de2aec74eb2a040012f5d0ae7d6b1ca7be9073d
tiger <13811402285@163.com>
2025-05-18 02:09:07 +0800

    init route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function App() {
+export default function Dashboard() {
   return (
     <div>
-      <h1>Application</h1>
+      <h1>Dashboard</h1>
     </div>
   )
 }

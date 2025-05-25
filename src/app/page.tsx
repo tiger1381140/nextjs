476d686e95c81bffdc6e5a188d723dadbf295dd8
tiger <13811402285@163.com>
2025-05-25 12:54:45 +0800

    11_parallelRoute

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className='flex items-center text-white text-xl font-bold mt-3 h-15 rounded-lg bg-gray-500'>
+        @Children Page
+    </div>
+  )
+}
